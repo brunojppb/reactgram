6f5e82d5958710979d6ef302f0b36fa93fb956c8
Bruno Paulino <brunojppb@gmail.com>
2020-01-20 21:50:45 +0100

    update main picture and fix loader in safari

--- src/components/static/Layout.jsx
+++ src/components/static/Layout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Footer } from '../common/Footer';
+import { Link } from 'react-router-dom';
+import Routes from '../../Routes';
 
 export const Layout = ({children}) => {
 
   return(
     <React.Fragment>
       <div className="home-wrapper" style={{margin: 'auto', padding: 16, maxWidth: 935, gridGap: 16}}>
-        <h1>Reactgram</h1>
+        <h1>
+          <Link to={Routes.INDEX}>Reactgram</Link>
+        </h1>
         {children}
         <Footer/>
       </div>
