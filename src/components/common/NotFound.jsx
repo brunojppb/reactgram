18c80758e53e94d9b31fb88e76d8de1495dfdda5
Bruno Paulino <bruno@woombikes.com>
2020-01-27 15:35:47 +0100

    add nicer 404 page

--- src/components/common/NotFound.jsx
+++ src/components/common/NotFound.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import empty from "../../img/empty.svg";
+import {Layout} from '../static/Layout';
 
 export const NotFound = () => {
   return(
-    <div className="page-not-found-container">
-      <h1>Error 404</h1>
-      <h2>Páge not found</h2>
-    </div>
+    <Layout>
+      <div className="page-not-found-container" style={{textAlign: 'center'}}>
+        <img src={empty} alt="sobre" className="static-page-img" />
+        <h2 style={{marginTop: 32}}>Page not found</h2>
+        <p>Looks like this page was removed or doesn't exist</p>
+      </div>
+    </Layout>
   );
 };
 
